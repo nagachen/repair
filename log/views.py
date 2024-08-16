bcd48bf11c304aa172f15575d6a0e739c5c3147c
NAGACHEN-DESK\nagachen <birdann99@gmail.com>
2024-08-16 18:18:12 +0800

    修正 log/views.py的錯誤，增加基底頁面範本

--- log/views.py
+++ log/views.py
@@ -4,10 +4,21 @@ from django.urls import reverse
 from .models import LogItem
 # Create your views here.
 
+#默认模板路径约定：
+#app_name/model_name_list.html: 对于 ListView，Django 会默认寻找这个路径下的模板。
+# 例如，你的 LogList 视图会寻找 log/logitem_list.html 这个模板。
+#app_name/model_name_form.html: 对于 CreateView 和 UpdateView，Django 
+# 会默认寻找这个路径下的模板。例如，你的 LogCreate 视图会寻找 log/logitem_form.html 这个模板。
+
+# 報修項目列表
 class LogList(ListView):
     model = LogItem
     ordering = ['-id']
-    
+
+# 檢視報修項目
+class LogView(DetailView):
+  model = LogItem
+  
 class LogCreate(CreateView):
     model = LogItem
     # 新增時只顯示需填寫的部份欄位
